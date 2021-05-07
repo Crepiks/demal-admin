6834b02e010fe1a87e25b29784e67150a5755167
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-05-07 14:18:25 +0600

    feat: create login page

---
+++ src/components/common/demal-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="button"><slot></slot></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/variables.scss";
+
+.button {
+  border: 1px solid $primary;
+  padding: 10px 10px;
+  border-radius: 4px;
+  background-color: $primary;
+  width: 100%;
+  color: $white;
+  text-align: center;
+  outline: none;
+  cursor: pointer;
+  transition: 200ms ease-in-out;
+
+  &:hover {
+    opacity: 0.8;
+  }
+}
+</style>
--- src/views/login/demal-login.vue
+++ src/views/login/demal-login.vue
@@ -1,9 +1,75 @@
 <template>
-  <div>login</div>
+  <div class="login">
+    <form action="" @submit.prevent="login" class="login-form">
+      <i class="fas fa-map-signs login-logo"></i>
+      <div class="login-subtitle">ADMIN</div>
+      <h2 class="login-title">Войти в аккаунт</h2>
+      <demalInput class="login-input" placeholder="Введите почту" />
+      <demalInput
+        class="login-input"
+        type="password"
+        placeholder="Введите пароль"
+      />
+      <demalButton class="login-button">Войти</demalButton>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+import demalInput from "@/components/common/demal-input";
+import demalButton from "@/components/common/demal-button";
+
+export default {
+  components: {
+    demalInput,
+    demalButton,
+  },
+};
 </script>
 
-<style></style>
+<style scoped lang="scss">
+@import "@/assets/styles/variables.scss";
+@import url("https://fonts.googleapis.com/css2?family=PT+Sans+Caption&display=swap");
+
+.login {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: $black;
+
+  &-logo {
+    color: $white;
+    margin-bottom: 30px;
+  }
+
+  &-title {
+    color: $white;
+    margin-bottom: 8px;
+    opacity: 0.8;
+    font-family: "PT Sans Caption", sans-serif !important;
+  }
+
+  &-subtitle {
+    font-size: 9px;
+    color: $white;
+    opacity: 0.5;
+    margin-bottom: 10px;
+  }
+
+  &-form {
+    width: 300px;
+    box-sizing: border-box;
+    padding: 30px 20px;
+  }
+
+  &-input {
+    margin-top: 10px;
+  }
+
+  &-button {
+    margin-top: 10px;
+  }
+}
+</style>
